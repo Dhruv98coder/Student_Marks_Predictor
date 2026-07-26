28637681051531414b07a83d11fac124b3d93c6d
Dhruv98coder <sharmadhruv9867@gmail.com>
2026-07-26 12:22:10 +0530

    Set DEBUG to False and update ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hx(4^1gskgz(j1cf97!u)_9s#8(=ob=30=xv!*!laq%g4k&)0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
